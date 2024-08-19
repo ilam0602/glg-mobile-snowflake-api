0fc0cd5bea4655f4ff1691e2cb9912e6fea2273a
ilam0602 <isaac.lam@live.com>
2024-08-19 12:59:54 -0700

    catch up with live server

--- flask_server_glg_app_apis.py
+++ flask_server_glg_app_apis.py
@@ -135,4 +135,4 @@ def get_debts():
     return json_data, code
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug = False,host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
